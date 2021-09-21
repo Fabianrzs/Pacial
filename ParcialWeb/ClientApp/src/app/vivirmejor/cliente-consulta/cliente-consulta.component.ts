0c2f55ab0222abcf8eb829743cb042b4f6e059f1
Fdamirez <fdramirez@unicesar.edu.co>
2021-09-21 09:45:57 -0500

    Punto 3 Corregido

--- ParcialWeb/ClientApp/src/app/vivirmejor/cliente-consulta/cliente-consulta.component.ts
+++ ParcialWeb/ClientApp/src/app/vivirmejor/cliente-consulta/cliente-consulta.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { AbonoService } from 'src/app/services/abono.service';
 import { ClienteService } from 'src/app/services/cliente.service';
+import { Abono } from '../models/abono';
 import { Cliente } from '../models/cliente';
 
 @Component({
@@ -11,21 +12,27 @@ import { Cliente } from '../models/cliente';
 export class ClienteConsultaComponent implements OnInit {
 
   clientes: Cliente[];
+  abonos: Abono[];
   
 
   constructor(private serviceCliente:ClienteService, 
-    private serviceAbonoService:AbonoService) { }
+    private serviceAbono:AbonoService) { }
 
   ngOnInit() {
     this.getClientes();
+    this.getAbonos();
   }
 
   getClientes() {
     this.clientes = this.serviceCliente.get();
   }
 
+  getAbonos() {
+    this.abonos = this.serviceAbono.get();
+  }
+
   getAbono(id: string) {
-    return this.serviceAbonoService.calculoAbonos(id);
+    return this.serviceAbono.calculoAbonos(id);
   }
 
 

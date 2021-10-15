25def7c6cd686d5eaf639ded02eb169b162f5ef0
Fdamirez <fdramirez@unicesar.edu.co>
2021-10-14 22:59:58 -0500

    Controladores sin terminar :)

---
+++ ParcialWeb/Controllers/ProyectoController.cs
@@ -0,0 +1,26 @@
+﻿using BLL;
+using DAL;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace parcialWeb.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProyectoController : ControllerBase
+    {
+        private ProyectoService service;
+
+        public ProyectoController(ProyectoContext context)
+        {
+            service = new ProyectoService(context);
+        }
+
+
+
+    }
+}
---
+++ ParcialWeb/Models/ProyectoModels.cs
@@ -0,0 +1,58 @@
+﻿using Entity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace parcialWeb.Models
+{
+    public class ProyectoViviendaInputModel
+    {
+        public string Nombre { get; set; }
+        public decimal Valor { get; set; }
+        public decimal CuotaInicial { get; set; }
+    }
+    public class ProyectoViviendaViewModel : ProyectoViviendaInputModel
+    {
+        public int CodProyecto { get; set; }
+        public ProyectoViviendaViewModel(ProyectoVivienda proyecto)
+        {
+            CodProyecto = proyecto.CodProyecto;
+            Nombre = proyecto.Nombre;
+            CuotaInicial = proyecto.CuotaInicial;
+        }
+    }
+
+    public class ClienteInputModel
+    {
+        public string Identificacion { get; set; }
+        public string Nombre { get; set; }
+        public int CodProyecto { get; set; }
+    }
+    public class ClienteViewModel : ClienteInputModel
+    {
+        public ClienteViewModel(Cliente cliente)
+        {
+            Identificacion = cliente.Identificacion;
+            Nombre = cliente.Nombre;
+            CodProyecto = cliente.CodProyecto;
+        }
+    }
+
+    public class AbonoInputModel
+    {
+        public decimal Valor { get; set; }
+        public string IdCliente { get; set; }
+    }
+    public class AbonoViewModel : AbonoInputModel
+    {
+        public int CodAbono { get; set; }
+
+        public AbonoViewModel(Abono abono)
+        {
+            CodAbono = abono.CodAbono;
+            Valor = abono.Valor;
+            IdCliente = abono.IdCliente;
+        }
+    }
+}

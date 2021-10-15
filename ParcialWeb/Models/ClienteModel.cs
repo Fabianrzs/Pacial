863f9fac90085b4d63ef7f20d910a589d6907348
Fdamirez <fdramirez@unicesar.edu.co>
2021-10-14 22:48:33 -0500

    Modelos Finalizados

---
+++ ParcialWeb/Models/ClienteModel.cs
@@ -0,0 +1,24 @@
+﻿using Entity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace parcialWeb.Models
+{
+    public class ClienteInputModel
+    {
+        public string Identificacion { get; set; }
+        public string Nombre { get; set; }
+        public int CodProyecto { get; set; }
+    }
+    public class ClienteViewModel : ClienteInputModel
+    {
+        public ClienteViewModel(Cliente cliente)
+        {
+            Identificacion = cliente.Identificacion;
+            Nombre = cliente.Nombre;
+            CodProyecto = cliente.CodProyecto;
+        }
+    }
+}

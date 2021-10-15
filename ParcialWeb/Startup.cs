a847b202c9398f9dc64fd81d8586eeef147562bc
Fdamirez <fdramirez@unicesar.edu.co>
2021-10-15 10:34:29 -0500

    Swagger Inicializado

--- ParcialWeb/Startup.cs
+++ ParcialWeb/Startup.cs
@@ -32,11 +32,27 @@ namespace ParcialWeb
             {
                 configuration.RootPath = "ClientApp/dist";
             });
+
+            services.AddSwaggerGen();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
+            app.UseSwagger();
+
+            // Enable middleware to serve swagger-ui (HTML, JS, CSS, etc.)
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "My API V1");
+            });
+
+            app.UseRouting();
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
